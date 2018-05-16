b84f1749d66c5410bfb1e7bb18265ad83948214c
Ojaswi Kumar <ojaswi.kumar@outlook.com>
2018-05-16 23:32:56 +0530

    Create First Activity

--- HomeSec/app/src/main/java/com/jones/ojaswikumar/homesec/MainActivity.java
+++ HomeSec/app/src/main/java/com/jones/ojaswikumar/homesec/MainActivity.java
@@ -1,13 +1,34 @@
 package com.jones.ojaswikumar.homesec;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnPaired;
+    ListView devicelist;
+
+    private BluetoothAdapter myBluetooth = null;
+    private Set<BluetoothDevice> pairedDevices;
+    public static String EXTRA_ADDRESS = "device_address";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+
+
+
 }
