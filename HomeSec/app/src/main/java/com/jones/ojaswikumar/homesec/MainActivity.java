d53be56e2212151ef3d4b6b14c90ab4345cec1eb
Ojaswi Kumar <ojaswi.kumar@outlook.com>
2018-05-18 23:57:16 +0530

    Find all paired Devices

--- HomeSec/app/src/main/java/com/jones/ojaswikumar/homesec/MainActivity.java
+++ HomeSec/app/src/main/java/com/jones/ojaswikumar/homesec/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -28,7 +29,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnPaired = (Button)findViewById(R.id.button);
+        devicelist = (ListView) findViewById(R.id.listView);
+        //if the device has bluetooth
+        myBluetooth = BluetoothAdapter.getDefaultAdapter();
 
+        if(myBluetooth == null)
+        {
+            //Show a mensag. that the device has no bluetooth adapter
+            Toast.makeText(getApplicationContext(), "Bluetooth Device Not Available", Toast.LENGTH_LONG).show();
+
+            //finish apk
+            finish();
+        }
+        else if(!myBluetooth.isEnabled())
+        {
+            //Ask to the user turn the bluetooth on
+            Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnBTon,1);
+        }
+
+        btnPaired.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Log.i("Log","Button Pressed");
+                pairedDevicesList();
+            }
+        });
+    }
+
+
+    private void pairedDevicesList()
+    {
+        pairedDevices = myBluetooth.getBondedDevices();
+        ArrayList list = new ArrayList();
+        //Log.i("PairedDevices",pairedDevices.toString());
+        if (pairedDevices.size()>0)
+        {
+            for(BluetoothDevice bt : pairedDevices)
+            {
+                list.add(bt.getName() + "\n" + bt.getAddress()); //Get the device's name and the address
+            }
+            Log.i("List",list.toString());
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "No Paired Bluetooth Devices Found.", Toast.LENGTH_LONG).show();
+        }
+
+        final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+        devicelist.setAdapter(adapter);
+    }
 
 
 }
